371a2e3d91d75a816e7582ea8b5bce3fd0d80c07
Cyrille <cyrillechateau@icloud.com>
2023-03-13 22:22:40 +0100

    update

--- programmeName.js
+++ programmeName.js
@@ -1,4 +1,10 @@
-const path = require('path');
 
-const filename = path.basename(process.argv[1]);
-console.log(filename);
+function pathname(){
+
+  const path = require('path');
+
+  const filename = path.basename(process.argv[1]);
+  console.log(filename);
+}
+
+pathname();
